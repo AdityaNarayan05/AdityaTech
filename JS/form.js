de6eaa609f395d8aa79947b087b4ccd872c95761
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-06-29 20:11:05 +0530

    Structured Project

--- JS/form.js
+++ JS/form.js
@@ -1,18 +1,3 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
 function validateform() {
   var name = document.contactform.name.value;
   var subject = document.contactform.subject.value;
